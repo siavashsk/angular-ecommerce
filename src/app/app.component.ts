78bb666959a61bdade127eb2b45293442633c978
siyavash <siavash.khani200@gmail.com>
2025-03-17 16:14:28 +0330

    feat(app/routes): implemented cart route

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
-import { ProductsListComponent } from './pages/products-list/products-list.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, ProductsListComponent],
-  template: `<app-header />
-    <app-products-list /> `,
+  imports: [HeaderComponent, RouterOutlet],
+  template: `<app-header /> <router-outlet /> `,
   styles: [],
 })
 export class AppComponent {
