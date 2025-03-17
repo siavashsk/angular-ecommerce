8874d35f995def8ec64a661037138ebf83f8c1ba
siyavash <siavash.khani200@gmail.com>
2025-03-17 14:40:31 +0330

    initial commit

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,15 @@
+import { Component, signal } from '@angular/core';
+import { PrimaryButtonComponent } from '../primary-button/primary-button.component';
+
+@Component({
+  selector: 'app-header',
+  imports: [PrimaryButtonComponent],
+  template: `
+    <div class="bg-slate-100 px-4 py-3 shadow-md flex justify-between">
+      <span>My Store</span>
+      <app-primary-button label="Cart" />
+    </div>
+  `,
+  styles: ``,
+})
+export class HeaderComponent {}
