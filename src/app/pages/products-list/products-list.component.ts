4fdcc6e0ddc31b154659f416af788976fb4c336c
siyavash <siavash.khani200@gmail.com>
2025-04-06 12:23:04 +0330

    feat(services): updated data, requested to json-server

--- src/app/pages/products-list/products-list.component.ts
+++ src/app/pages/products-list/products-list.component.ts
@@ -20,33 +20,8 @@ export class ProductsListComponent {
   async ngOnInit() {
     const res = await fetch('http://localhost:3000/products');
     const data = await res.json();
-    console.log(data);
-    // this.products.set(data);
+    this.products.set(data);
   }
 
-  products = signal<Product[]>([
-    {
-      id: 1,
-      title: 'Fjallraven',
-      price: 100.0,
-      image:
-        'https://images.pexels.com/photos/90946/pexels-photo-90946.jpeg?cs=srgb&dl=pexels-madebymath-90946.jpg&fm=jpg',
-      stock: 10,
-    },
-    {
-      id: 2,
-      title: 'Mens Casual Premium Slim Fit T-Shirts',
-      price: 100.0,
-      image:
-        'https://images.unsplash.com/photo-1523275335684-37898b6baf30?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZHVjdHxlbnwwfHwwfHx8MA%3D%3D',
-    },
-    {
-      id: 3,
-      title: 'Mens Cotton Jacket',
-      price: 100.0,
-      image:
-        'https://images.rawpixel.com/image_800/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDIyLTExL3JtMzYyLTAxYS1tb2NrdXAuanBn.jpg',
-      stock: 4,
-    },
-  ]);
+  products = signal<Product[]>([]);
 }
