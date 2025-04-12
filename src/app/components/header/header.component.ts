cd8d83c40c0f74287e683bb8d56ef95dd9b03247
siyavash <siavash.khani200@gmail.com>
2025-04-12 15:00:02 +0330

    feat(header): added dropdown menu

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,12 @@
-import { Component, inject } from '@angular/core';
+import { Component, ElementRef, inject } from '@angular/core';
 import { PrimaryButtonComponent } from '../primary-button/primary-button.component';
 import { CartService } from '../../services/cart.service';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
-  imports: [PrimaryButtonComponent, RouterLink],
+  imports: [PrimaryButtonComponent, RouterLink, CommonModule],
   template: `
     <div
       class="bg-slate-100 px-4 py-3 shadow-md flex justify-between items-center"
@@ -17,23 +18,48 @@ import { RouterLink } from '@angular/router';
           (btnClicked)="showButtonClicked()"
           routerLink="/cart"
         />
-        <button
-          class="text-xl cursor-pointer border py-1 rounded-xl"
-          routerLink="/profile"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="40"
-            height="30"
-            fill="currentColor"
-            class="bi bi-person"
-            viewBox="0 0 16 16"
+        <div class="relative">
+          <button
+            class="text-xl cursor-pointer border py-1 rounded-xl"
+            (click)="toggleDropdown()"
           >
-            <path
-              d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z"
-            />
-          </svg>
-        </button>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="40"
+              height="30"
+              fill="currentColor"
+              class="bi bi-person"
+              viewBox="0 0 16 16"
+            >
+              <path
+                d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z"
+              />
+            </svg>
+          </button>
+          <div
+            *ngIf="dropdownVisible"
+            class="absolute right-0 z-40 mt-6 bg-white border border-gray-300 rounded-lg shadow-lg w-[150px] text-center"
+          >
+            <button
+              class="w-full cursor-pointer py-2 text-zinc-800 hover:bg-gray-200"
+              routerLink="/profile"
+            >
+              My Profile
+            </button>
+            <button
+              class="w-full cursor-pointer py-2 text-zinc-800 hover:bg-gray-200"
+              routerLink="/notifications"
+            >
+              Notifications
+            </button>
+            <button
+              class="w-full cursor-pointer py-2 text-red-700 hover:bg-red-200"
+              (click)="logout()"
+            >
+              Logout
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   `,
@@ -41,8 +67,35 @@ import { RouterLink } from '@angular/router';
 })
 export class HeaderComponent {
   cartService = inject(CartService);
+  dropdownVisible = false;
+
+  constructor(private router: Router, private elRef: ElementRef) {}
+
+  ngAfterViewInit() {
+    document.addEventListener('click', this.closeDropdownIfOutside);
+  }
+  ngOnDestroy() {
+    document.removeEventListener('click', this.closeDropdownIfOutside);
+  }
+
+  closeDropdownIfOutside = (event: MouseEvent) => {
+    if (
+      this.dropdownVisible &&
+      !this.elRef.nativeElement.contains(event.target)
+    ) {
+      this.dropdownVisible = false;
+    }
+  };
 
   showButtonClicked() {
     console.log('clicked');
+  }
+
+  toggleDropdown() {
+    this.dropdownVisible = !this.dropdownVisible;
+  }
+
+  logout() {
+    this.router.navigate(['/login']);
   }
 }
